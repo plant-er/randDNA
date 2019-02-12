cdddb5482e5a078f0413ca66597e94b286fc4dbd
plant-er <plant-er@users.noreply.github.com>
2019-02-11 20:01:49 -0400

    Added some code to header file

---
+++ src/rdna.hpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<random>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string randDNA(int seed, string bases, int n)
+	{
+		std::random_device seed;
+		
+		std::mt19937 eng1(seed());
+		
+	}
+	
+	return 0;
+}
