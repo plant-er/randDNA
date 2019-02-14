70def0ae823725554cb5101acbcb610449ab3654
plant-er <plant-er@users.noreply.github.com>
2019-02-13 20:07:58 -0400

    Changed header file again

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<random>
 #include<string>
-//using namespace std;
+#include<random>
+#include<iostream>
+using namespace std;
 
 
 string randDNA(int seed, string bases, int n){
@@ -9,13 +9,13 @@ string randDNA(int seed, string bases, int n){
 		
 		std::mt19937 eng1(seed);
 	
-		std::uniform_int_distribution<> unifrm(0,n);
+		std::uniform_int_distribution<int> unifrm(0,n);
 		
 	
 		
-		std::cout<<unifrm(eng1)<<" ";
+		cout<<unifrm(eng1)<<" ";
 		
-			return randDNA;
+			return " ";
 	}
 	
 	
