fbff5c62470ab2e61d87d5998ebdc9db470578c1
plant-er <plant-er@users.noreply.github.com>
2019-02-18 19:30:26 -0400

    More changes to Header file

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -8,12 +8,16 @@ string randDNA(int seed, string bases, int n){
 	
 		
 		std::mt19937 eng1(seed);
-	
-		std::uniform_int_distribution<int> unifrm(0,n);
+		std::uniform_int_distribution<int> unifrm(0,3);
 		
+		char mybases[4] ={'A','C','G','T'};
 	
+		for(int i=1; i < n; i++)
+		{
 		
-		cout<<unifrm(eng1)<<" ";
+		int value = unifrm(eng1);
+		bases = bases + mybases[value];
+		}
 		
 			return " ";
 	}
